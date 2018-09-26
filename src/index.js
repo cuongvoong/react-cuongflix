2bde4e7cd4f456416c6dc89a04c80df6098e1a9b
cuong <cuongvoong@gmail.com>
2018-09-25 17:24:51 -0700

    removed dotenv

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./containers/App";
 import registerServiceWorker from "./registerServiceWorker";
 import store from "./store";
-require("dotenv").config();
 
 ReactDOM.render(<App store={store} />, document.getElementById("root"));
 registerServiceWorker();
