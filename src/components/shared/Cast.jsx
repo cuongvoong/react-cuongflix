a1a241a67aed9aa650cd2eb650bc24473fa7f148
cuong <cuongvoong@gmail.com>
2018-10-03 20:09:02 -0700

    fixed css for movie/tv pages

--- src/components/shared/Cast.jsx
+++ src/components/shared/Cast.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Cast.css";
-import Card from "./Card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 const Cast = ({ cast }) => {
-  return <Card castMember={cast} />;
+  return (
+    <section className="cast-item">
+      <section className="card">
+        {cast.profile_path !== null ? (
+          <img
+            src={`https://image.tmdb.org/t/p/w138_and_h175_face${
+              cast.profile_path
+            }`}
+            alt=""
+          />
+        ) : (
+          <div className="no-profile">
+            <FontAwesomeIcon icon={faUser} size="6x" />
+          </div>
+        )}
+        <section className="name">{cast.name}</section>
+        <section className="character">{cast.character}</section>
+      </section>
+    </section>
+  );
 };
 
 Cast.propTypes = {
--- src/components/shared/Credits.jsx
+++ src/components/shared/Credits.jsx
@@ -1,111 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import "./Credits.css";
 import Cast from "./Cast";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faChevronLeft,
-  faChevronRight
-} from "@fortawesome/free-solid-svg-icons";
 
-class Credits extends Component {
-  constructor(props) {
-    super(props);
+const Credits = ({ credits }) => {
+  const cast = credits.cast.map(member => {
+    return <Cast key={member.credit_id} cast={member} />;
+  });
 
-    this.cast = React.createRef();
-    this.sliderAnimation = null;
-  }
-
-  state = {
-    hasMovedOnce: false,
-    moves: 0,
-    animating: false,
-    width: 0,
-    pages: 0
-  };
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.credits.cast !== this.props.credits.cast) {
-      const width = this.cast.current.offsetWidth;
-      const pages =
-        Math.floor(
-          (Object.keys(this.props.credits.cast).length * 138) / width
-        ) + 1;
-      this.setState({ width, pages });
-    }
-  }
-
-  advancePrev = () => {
-    if (!this.state.animating) {
-      this.setState({ animating: true }, () => {
-        const translatePercent = (this.state.moves - 1) * 100;
-        this.cast.current.style.transform = `translate3d(-${translatePercent}%,0,0)`;
-        this.sliderAnimation = setTimeout(() => {
-          const moves = this.state.moves === 0 ? 0 : this.state.moves - 1;
-          this.setState({
-            animating: false,
-            hasMovedOnce: true,
-            moves
-          });
-        }, 750);
-      });
-    }
-  };
-
-  advanceNext = () => {
-    if (!this.state.animating) {
-      const translatePercent = (this.state.moves + 1) * 100;
-      this.cast.current.style.transform = `translate3d(-${translatePercent}%,0,0)`;
-      this.setState({ animating: true }, () => {
-        this.sliderAnimation = setTimeout(() => {
-          this.setState({
-            animating: false,
-            hasMovedOnce: true,
-            moves: this.state.moves + 1
-          });
-        }, 750);
-      });
-    }
-  };
-
-  render() {
-    const { credits } = this.props;
-
-    const cast = credits.cast.map(member => {
-      return <Cast key={member.credit_id} cast={member} />;
-    });
-
-    const castClasses = "cast" + (this.state.animating ? " animating" : "");
-
-    return (
-      <section className="credits">
-        <h3>Cast</h3>
-        <div className="cast-container">
-          {this.state.hasMovedOnce &&
-            this.state.moves !== 0 && (
-              <span
-                onClick={() => this.advancePrev()}
-                className="handle handlePrev"
-              >
-                <FontAwesomeIcon icon={faChevronLeft} size="3x" />
-              </span>
-            )}
-          <section className={castClasses} ref={this.cast}>
-            {cast}
-          </section>
-          {!(this.state.moves + 1 >= this.state.pages) && (
-            <span
-              onClick={() => this.advanceNext()}
-              className="handle handleNext"
-            >
-              <FontAwesomeIcon icon={faChevronRight} size="3x" />
-            </span>
-          )}
-        </div>
-      </section>
-    );
-  }
-}
+  return (
+    <section className="credits">
+      <h4>Cast</h4>
+      <section className="cast">{cast}</section>
+    </section>
+  );
+};
 
 Credits.propTypes = {
   credits: PropTypes.object.isRequired
